4027765c249ce8f6fd086322be227bd2800bdcc8
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-08-11 21:36:53 -0400

    Refractor the file storage code

--- models/product.js
+++ models/product.js
@@ -7,28 +7,28 @@ const p = path.join(
   'products.json'
 );
 
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, data) => {
+    if (err) {
+      return cb([]);
+    }
+    cb(JSON.parse(data));
+  });
+};
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    fs.readFile(p, (err, data) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(data);
-      }
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => console.log(err));
     });
   }
 
   static fetchAll(cb) {
-    fs.readFile(p, (err, data) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(data));
-    });
+    getProductsFromFile(cb);
   }
 };
