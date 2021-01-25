6985982c8a77a19b04f5193ce3988fb307bd34e6
Charles-Hebert Dalzon <charlby5@gmail.com>
2021-01-25 15:14:53 -0500

    Add a tiny improvements

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,19 +24,20 @@ exports.postSignUp = (req, res, next) => {
   User.findOne({ email })
     .then((existingUser) => {
       if (existingUser) {
-        res.redirect('/signup');
+        return res.redirect('/signup');
       }
-      return bcrypt.hash(password, 12);
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => res.redirect('/login'));
     })
-    .then((hashedPassword) => {
-      const user = new User({
-        email,
-        password: hashedPassword,
-        cart: { items: [] },
-      });
-      return user.save();
-    })
-    .then((result) => res.redirect('/login'))
     .catch((err) => console.log(err));
 };
 
